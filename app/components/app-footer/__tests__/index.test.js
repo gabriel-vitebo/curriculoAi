085662cff783cbc0db5f61835851531c42a56871
gabriel-vitebo <vitebogabriel@gmail.com>
2026-03-15 00:13:25 -0300

    test: update AppFooter and AppHeader snapshots to use dynamic app version

--- app/components/app-footer/__tests__/index.test.js
+++ app/components/app-footer/__tests__/index.test.js
@@ -5,11 +5,12 @@ import AppFooter from '../index.vue'
 describe('AppFooter', () => {
   it('renderiza os links do rodape e a versao', async () => {
     const wrapper = await mountSuspended(AppFooter)
+    const versionText = wrapper.find('footer > span:last-child').text()
 
     expect(wrapper.text()).toContain('GitHub')
     expect(wrapper.text()).toContain('Releases')
-    expect(wrapper.text()).toContain('Versao 1.0')
+    expect(versionText).toMatch(/^(v\d+\.\d+\.\d+|dev-[0-9a-f]+)$/)
     expect(wrapper.find('a[href="https://github.com/gabriel-vitebo/curriculoAi"]').exists()).toBe(true)
-    expect(wrapper.html()).toMatchSnapshot()
+    expect(wrapper.html().replace(versionText, '__APP_VERSION__')).toMatchSnapshot()
   })
 })
